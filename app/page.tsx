f22feb5c2252a120c934e9a158d3f331995e8c5f
Aesthetic Interior <aestheticinterior@Aesthetics-Mac-mini.local>
2026-05-21 15:39:21 +0600

    Solving the error of the websites

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { HomeHeroSection } from "@/components/website/homePage/home-hero-section";
 import { ProcessSection } from "@/components/website/homePage/process-section";
 import { ServicesSection } from "@/components/website/homePage/services-section";
-import { ProjectSection } from "@/components/website/homePage/projects-section";
+
 import { TrustFiguresSection } from "@/components/website/homePage/trust-figure-section";
 import { PartnersSection } from "@/components/website/homePage/partners-section";
 import { VideoGallerySection } from "@/components/website/homePage/video-gallery-section";
