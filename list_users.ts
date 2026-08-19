74a2e4a7ba74bc67c1986d25cbabc65453dc81ff
mdalraihan464-dev <mdalraihan464@gmail.com>
2026-08-19 14:11:16 +0600

    Fix Prisma client import in user listing script

--- list_users.ts
+++ list_users.ts
@@ -1,16 +1,17 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from './lib/prisma'
 
 async function main() {
   const users = await prisma.user.findMany({
     include: {
       userRoles: {
         include: {
-          role: true
-        }
-      }
-    }
+          role: true,
+        },
+      },
+    },
   })
+
   console.log(JSON.stringify(users, null, 2))
 }
+
 main()
